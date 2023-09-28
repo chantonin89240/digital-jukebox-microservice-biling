2967bbf5888976349e9caeff56e8a53252ec6ee7
AlvesLucas5483 <90681993+AlvesLucas5483@users.noreply.github.com>
2023-09-28 14:52:29 +0200

    began work on implementing Stripe

---
+++ Micro-service-biling/Web/Controllers/BillingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("Billing")]
+    public class BillingController : Controller
+    {
+        public BillingController() { }
+
+        /// <summary>
+        /// Creates a Billing
+        /// </summary>
+        [Route("/Create")]
+        [HttpPost]
+        public void CreateBilling() { 
+            
+        }
+    }
+}
